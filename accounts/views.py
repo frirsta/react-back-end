eb0e14d67900146ba3aeee8d2a68680b984174d2
frirsta <frista-8@hotmail.com>
2023-02-22 11:48:52 +0000

    Add AccountDetail class to views

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,14 @@
 from .models import Account
 from .serializers import AccountsSerializer
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
 
 
 class AccountList(ListAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountsSerializer
+
+
+class AccountDetail(RetrieveAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountsSerializer

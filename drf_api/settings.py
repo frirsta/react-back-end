6dd6b13690364f021e8fe3d9f0606719d84d9f3b
frirsta <frista-8@hotmail.com>
2023-02-25 18:54:22 +0000

    Refactor code to follow PEP8 style guide

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -39,7 +39,9 @@ JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_SECURE = True
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 
-REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'}
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+    }
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/

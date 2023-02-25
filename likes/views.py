940cf4e69a90d57e2d817b442b23c80c78c6e0d9
frirsta <frista-8@hotmail.com>
2023-02-25 18:06:28 +0000

    Add filterset_fields to LikeList

--- likes/views.py
+++ likes/views.py
@@ -1,4 +1,6 @@
-from rest_framework import permissions
+from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions, filters
 from rest_framework.generics import (
     ListCreateAPIView, RetrieveDestroyAPIView)
 from drf_api.permissions import OwnerOrReadOnly
@@ -8,11 +10,22 @@ from .serializers import LikeSerializer
 
 class LikeList(ListCreateAPIView):
     """
-    Displays a list of all the Likes and their information.
+    Displays a list of all the likes and their information.
+    The filterset_fields can find likes made by a specific user
+    The filterset_fields can also find what likes have been made
+    on a specific post.
     """
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Like.objects.all()
+    queryset = Like.objects.annotate(
+        like_count=Count('owner__like', distinct=True),
+    ).order_by('-created_date')
     serializer_class = LikeSerializer
+    filter_backends = [
+        DjangoFilterBackend]
+    filterset_fields = [
+        'post',
+        'owner__like__owner__account'
+        ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
